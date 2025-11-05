6b15144d1ff5d1a3f3e44666e88c7d4b2ab39f92
Poottichai-Poom <poottichai05012549@gmail.com>
2025-11-05 09:12:52 +0700

    11/5

---
+++ src/test/java/com/project_hackathon/firstapp/FirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_hackathon.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
